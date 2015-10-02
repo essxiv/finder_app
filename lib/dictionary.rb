7a175925f00a87986e259a32eb0424241620bf33
essxiv <essxiv@gmail.com>
2015-10-02 16:10:54 -0700

    init proj

---
+++ lib/dictionary.rb
@@ -0,0 +1,10 @@
+class Dictionary
+
+    @@filepath = nil
+
+    def self.filepath=()
+    end
+
+    def self.file_exist?
+    end
+end
---
+++ lib/guide.rb
@@ -0,0 +1,28 @@
+require 'dictionary'
+
+class Guide
+
+    def initialize()
+    end
+
+    def launch!
+    end
+
+    def get_action
+    end
+
+    def do_action
+    end
+
+    def list
+    end
+
+    def add
+    end
+
+    def introduction
+    end
+
+    def conclusion
+    end
+end
