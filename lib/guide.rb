340780072bd061d8677fde95117b06d69776154b
essxiv <essxiv@gmail.com>
2015-10-02 16:57:47 -0700

    Added guide init, launch, get_action, do_action, list, find

--- lib/guide.rb
+++ lib/guide.rb
@@ -2,6 +2,14 @@ require 'dictionary'
 
 class Guide
 
+    class Config
+        @@actions = ['list', 'find', 'add', 'quit']
+
+        def self.actions
+            @@actions
+        end
+    end
+
     def initialize(@path=nil)
         Dictionary.filepath = path
 
@@ -21,15 +29,55 @@ class Guide
     end
 
     def get_action
+        action = nil
+        until Guide::Config.actions.include?(action)
+            uts "Commands: " + Guide::Config.actions.join(", ") if action
+            print "> ".center(20)
+            user_response = gets.chomp
+            action = user_response.downcase.strip
+        end
+        return action
     end
 
     def do_action
+        case action
+        when 'list'
+            puts "Listing all.."
+            list
+        when 'find'
+            puts "Finding all.."
+            find
+        when 'add'
+            puts "Adding.."
+            add
+        when 'quit'
+            return :quit
+        else
+            puts "I Don't Understand that Command.."
+        end
     end
 
     def list
+        puts "Listing All.."
+        dictionary = Dictionary.saved_dictionary
+        dictionary.each do |dict|
+            puts dict.subject + " | " + dict.location + " | " + dict.description
+        end
     end
 
     def add
+        puts "Adding.."
+
+        dictionary = Dictionary.build_questions
+
+        if dictionary.save
+            puts "Saved to Dictionary!"
+        else
+            puts "Save Error: Not Added.."
+        end
+    end
+
+    def find
     end
 
     def introduction
